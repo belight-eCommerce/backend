bcd28c730dcf9241fc276824d8f8537e9157ae5e
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-06-19 22:52:20 +0300

    feat: scaffold core modules (auth, user, product, order, cart, payment) with controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { ProductModule } from './modules/product/product.module';
+import { OrderModule } from './modules/order/order.module';
+import { CartModule } from './modules/cart/cart.module';
+import { PaymentModule } from './modules/payment/payment.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, ProductModule, OrderModule, CartModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
