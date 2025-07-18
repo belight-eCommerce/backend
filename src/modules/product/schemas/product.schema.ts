48157e7a6e805c70094362cc4a3ee1898945bd19
HaileA2 <hailea536@gmail.com>
2025-07-18 18:33:46 +0200

    feat:added product shema

--- src/modules/product/schemas/product.schema.ts
+++ src/modules/product/schemas/product.schema.ts
@@ -1,1 +1,33 @@
-echo
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ProductDocument = Product & Document;
+
+@Schema({ timestamps: true })
+export class Product {
+  @Prop({ required: true })
+  name: string;
+  @Prop({ required: true })
+  description: string;
+  @Prop({ required: true })
+  price: number;
+  @Prop({ required: true })
+  category: string;
+  @Prop()
+  sub_category?: string;
+  @Prop({ type: [String], default: [] })
+  images: string[];
+  @Prop({ required: true, default: 0 })
+  stock_quantity: number;
+  @Prop()
+  brand?: string;
+  @Prop({ default: 0 })
+  ratings: number;
+  @Prop({ default: 0 })
+  num_reviews: number;
+  @Prop({ default: true })
+  is_available: boolean;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
+
